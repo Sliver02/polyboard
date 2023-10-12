b4fda7a8c1dde66dcb2d49cb1883a3190d1bdc45
Sliver02 <japo02@hotmail.it>
2023-10-12 21:56:58 +0200

    light intensity

--- src/components/LightBulb.tsx
+++ src/components/LightBulb.tsx
@@ -3,7 +3,7 @@ import { MeshProps } from "@react-three/fiber";
 const LightBulb = (props: MeshProps) => {
   return (
     <mesh {...props}>
-      <pointLight castShadow />
+      <pointLight castShadow intensity={15} />
       <sphereGeometry args={[0.2, 30, 10]} />
       <meshPhongMaterial emissive={"yellow"} />
     </mesh>
